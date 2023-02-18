c986e405e7509e1a8d1faa7513501e65e79cde7b
Aleksander Dreszler <olek.d93@gmail.com>
2023-02-18 21:16:09 +0100

    Wykonano zadanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,11 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
+        Room room1 = new Room(5, 21.5, 20, true);
+        System.out.println(room1.getRoomInfo());
+        System.out.println(room1.lowerTemperature());
+        System.out.println(room1.getRoomInfo());
+        System.out.println(room1.lowerTemperature());
+        System.out.println(room1.getRoomInfo());
     }
 }
